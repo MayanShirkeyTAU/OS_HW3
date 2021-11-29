25176799620c2628bc2a5fb973360b944c242c2d
mayanshirkeyTau <mayanshirkey@mail.tau.ac.il>
2021-11-29 12:44:06 +0200

    FIRST

---
+++ cmake-build-debug/CMakeFiles/OS_HW3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_HW3.dir/main.c.o"
+  "OS_HW3"
+  "OS_HW3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
